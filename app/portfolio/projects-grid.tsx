792cb958077b87658a5f1f556432965aea55c55e
Raksa <rkob7305@gmail.com>
2026-03-30 04:39:35 +0700

    Make search icon white

--- app/portfolio/projects-grid.tsx
+++ app/portfolio/projects-grid.tsx
@@ -76,6 +76,7 @@ function SearchIcon() {
       width={31}
       height={31}
       className="w-5 h-5 sm:w-6 sm:h-6 md:w-[31px] md:h-[31px]"
+      style={{ filter: 'brightness(0) invert(1)' }}
       aria-hidden="true"
     />
   )
